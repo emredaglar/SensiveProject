0cb9df9e87467ac23b4b4724c286d4fb3461454e
emredaglar <emre.daglar1@gmail.com>
2024-10-30 15:59:40 +0300

    added Identity

---
+++ SensiveProject.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SensiveProject.EntityLayer.Concrete;
+using SensiveProject.PresentationLayer.Models;
+
+namespace SensiveProject.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //register yapmak için kullanılan sınıf
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                Email = model.Email,
+                UserName = model.UserName,
+                ImageUrl = "test"
+            };
+            var result = await _userManager.CreateAsync(appUser, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
